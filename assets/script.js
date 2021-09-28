4e707ddc39196b4a3be59281ad68c75846b1fecf
Benjiman Sutton <benjimansutton@gmail.com>
2021-09-28 19:28:23 +0000

    Added 4 questions to js file and answers

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: 'What is the capital of Chile?',
+        answers: [
+            {text: 'Santiago', correct: true},
+            {text: 'London', correct: false},
+            {text: 'Mexico', correct: false},
+            {text: 'Columbia', correct: false}
+        ]
+    },
+    {
+        question: 'What is the highest mountain in Britain?',
+        answers: [
+            {text: 'Snowdon', correct: false},
+            {text: 'Llangollen', correct: false},
+            {text: 'Evererst', correct: false},
+            {text: 'Ben Nevis', correct: true}
+        ]
+    },
+    {
+        question: 'What is the smallest country in the world?',
+        answers: [
+            {text: 'Florida', correct: false},
+            {text: 'Vatican City', correct: true},
+            {text: 'Manchester', correct: false},
+            {text: 'Queensland', correct: false}
+        ]
+    },
+    {
+        question: 'What is the hottest continent on Earth?',
+        answers: [
+            {text: 'America', correct: false},
+            {text: 'Europe', correct: false},
+            {text: 'Asia', correct: false},
+            {text: 'Africa', correct: true}
+        ]
+    },
+]
